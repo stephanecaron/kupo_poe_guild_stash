762e5cb5b9059fbc914658ed7af68ebe1a127cb7
stephc <stephane.caron@groupesavoie.com>
2023-12-11 15:52:13 -0400

    created the trendline chart png maker, formated it for discord

--- elp.py
+++ elp.py
@@ -1,25 +1,45 @@
+import matplotlib
+matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
 import pandas as pd
 from mysql_db import sqlOperations, sqlPoolOpen
 
 
 connection = sqlPoolOpen.get_connection()
 cursor = connection.cursor()
-## get inventory_history from mySQL
 data = sqlOperations.get_inventory_history(cursor)
 df = pd.DataFrame(data, columns=['date','league','stash','item_name','quantity'] )
 df['date'] = pd.to_datetime(df['date'])
 
-alteration_orb_data = df[df['item_name'] == 'Portal Scroll']
-daily_sum = alteration_orb_data.groupby(df['date'].dt.strftime('%Y-%m-%d %H')).agg({'quantity': 'sum'}).reset_index()
+seeking = 'orb of Alteration' #input('Please enter the name of the item you\'re looking for: ')
 
-plt.figure(figsize=(10, 6))
-plt.plot(daily_sum['date'], daily_sum['quantity'], marker='o', linestyle='-', color='green', label='Portal Scroll')
-plt.title('Trendline of Sum of Quantity for "Portal Scroll" Over Time')
-plt.xlabel('Date and Hour')
-plt.ylabel('Sum of Quantity')
-plt.xticks(rotation=45)
-plt.grid(True, linestyle='--', alpha=0.6)
-plt.legend()
+currency_data = df[df['stash'].str.contains('Currency', case=False, na=False) & df['item_name'].str.contains('Orb of Alteration', case=False, na=False)]
+
+
+daily_sum = currency_data.groupby(['date', 'item_name']).agg({'quantity': 'sum'}).reset_index()
+
+daily_sum_pivot = daily_sum.pivot(index='date', columns='item_name', values='quantity').reset_index()
+daily_sum_pivot = daily_sum_pivot.fillna(0)
+plt.figure(figsize=(4, 3))
+
+for currency in daily_sum_pivot.columns[1:]:
+    plt.plot(
+        daily_sum_pivot['date'],
+        daily_sum_pivot[currency],
+        linestyle='-',     
+        linewidth=2,
+        label=currency,
+        color='orange',
+        alpha=1
+    )
+
+plt.title(f'Trend over time', color='white')
+plt.yticks([0, max(daily_sum_pivot[currency])], color='white')
+plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%b %d, %Y'))
+plt.xticks([min(daily_sum_pivot['date']),max(daily_sum_pivot['date'])], color='white')
+plt.grid(False)
+plt.legend().set_visible(False)
 plt.tight_layout()
-plt.show()
+plt.savefig('currency_plot.png', transparent=True, bbox_inches='tight', pad_inches=0)
+plt.close()
